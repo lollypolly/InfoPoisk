bb68fb12952343f3b5bad329f832d20e4d034133
rinat.r.ahmethanov <rinat.r.ahmethanov@tatar.ru>
2023-02-22 09:13:44 +0300

    task2.py READY

--- Task2/task2.py
+++ Task2/task2.py
@@ -1,5 +1,3 @@
-from bs4 import BeautifulSoup
-import spacy
 import ru_core_news_md
 import nltk
 
@@ -8,19 +6,19 @@ nltk.download('popular')
 
 tokens = set()
 lemmas = set()
+word_for_remove = ['и', 'или', 'в', 'а', 'из-за', 'из-под', 'над', 'да', 'если', 'что', 'когда', 'потому', 'зато', 'однако', 'либо', 'не', 'то', 'лишь', 'едва', 'так', 'же', 'также', 'ни', 'с', 'где']
 
 
 def fill_tokens(file_name):
-    functors_pos = {'CONJ', 'ADV-PRO', 'CONJ', 'PART'}
     print(file_name)
     file = open(file_name, encoding='utf-8').read()
     tmp_tokens = nltk.word_tokenize(file, language='russian')
     for t in tmp_tokens:
-        #https://ru.stackoverflow.com/questions/782994/%D0%A3%D0%B4%D0%B0%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5-%D0%B8%D0%B7-%D1%81%D1%82%D1%80%D0%BE%D0%BA%D0%B8-%D1%81%D0%BB%D1%83%D0%B6%D0%B5%D0%B1%D0%BD%D1%8B%D1%85-%D1%87%D0%B0%D1%81%D1%82%D0%B5%D0%B9-%D1%80%D0%B5%D1%87%D0%B8%D1%81%D0%BE%D1%8E%D0%B7%D1%8B-%D0%BF%D1%80%D0%B5%D0%B4%D0%BB%D0%BE%D0%B3%D0%B8-%D1%87%D0%B0%D1%81%D1%82%D0%B8%D1%86%D1%8B-%D0%BC%D0%B5%D0%B6%D0%B4%D0%BE%D0%BC%D0%B5%D1%82%D0%B8%D1%8F-%D0%B8-%D0%B4
         t = t.lower().strip()
-        if match(t) and not matchNotRussian(t) and len(t) >= 2:
+        if match(t) and not matchNotRussian(t) and len(t) >= 2 and t not in word_for_remove:
             tokens.add(t)
             lemmas.add(nlp(t)[0].lemma_)
+
 
 
 def match(text, alphabet=set('абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ')):
